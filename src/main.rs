4adc8031c60b3e6d1d6067a609eabd171304e223
Rohit Agarwal <rohit.net@gmail.com>
2025-02-09 11:31:53 -0800

    Respond to multiple PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -7,7 +7,18 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+                let mut buf = [0; 512];
+                loop {
+                    let read_count = stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
+                        break;
+                    }
+
+                    // "b" make it a byte string
+                    // "unwrap" assert that no errors occur during the write operation.
+                    // If an error does occur, it will cause the program to panic and exit.
+                    stream.write_all(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
