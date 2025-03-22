9931963c920ee586d36d4befbe1f0e78b06389ef
Rohit Agarwal <rohit.net@gmail.com>
2025-03-22 09:16:44 -0700

    refactor Set

--- src/command/set.rs
+++ src/command/set.rs
@@ -6,19 +6,29 @@ impl Command {
     pub fn set(&self, storage: &mut Db) -> RespData {
         let key = self.args[0].clone().unpack_str();
         let value = self.args[1].clone().unpack_str();
+        let expires = self.calculate_expires();
 
-        let expires: u128 = if self.args.len() < 4 {
-            0
-        } else {
-            let expiry_mode = self.args[2].clone().unpack_str();
-            match expiry_mode.to_lowercase().as_str() {
-                "px" => self.args[3].clone().unpack_str().parse::<u128>().unwrap(),
-                _ => return RespData::Error("ERR invalid expiry mode".into()),
-            }
-        };
+        if let Err(err) = expires {
+            return RespData::Error(err);
+        }
 
-        println!("Setting key: {key}, value: {value}, expires: {expires}");
-        storage.set(key, value, expires);
+        storage.set(key, value, expires.unwrap());
         RespData::SimpleString("OK".to_string())
+    }
+
+    fn calculate_expires(&self) -> Result<u128, String> {
+        if self.args.len() < 4 {
+            return Ok(0);
+        }
+
+        let expiry_mode = self.args[2].clone().unpack_str();
+        match expiry_mode.to_lowercase().as_str() {
+            "px" => self.args[3]
+                .clone()
+                .unpack_str()
+                .parse::<u128>()
+                .map_err(|_| "ERR invalid expiry value".into()),
+            _ => Err("ERR invalid expiry mode".into()),
+        }
     }
 }
