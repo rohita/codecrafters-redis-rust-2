77c80de16242980b34448676ae57abe41c026846
Rohit Agarwal <rohit.net@gmail.com>
2025-03-22 19:02:52 -0700

    codecrafters submit [skip ci]

--- src/command/info.rs
+++ src/command/info.rs
@@ -2,11 +2,18 @@ use crate::command::Command;
 use crate::resp::RespData;
 use crate::storage::Db;
 
+const DEFAULT_MASTER_REPLID: &str = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+const DEFAULT_MASTER_OFFSET: &str = "0";
+
 impl Command {
     pub fn info(&self, storage: &Db) -> RespData {
-        match storage.config_get(&"replicaof".to_string()) {
-            Some(_v) => RespData::BulkString("role:slave".to_string()),
-            None => RespData::BulkString("role:master".to_string()),
-        }
+        let role = match storage.config_get(&"replicaof".to_string()) {
+            Some(_v) => "slave",
+            None => "master",
+        };
+
+        RespData::BulkString(
+            format!("role:{}\r\nmaster_replid:{}\r\nmaster_repl_offset:{}", role, DEFAULT_MASTER_REPLID, DEFAULT_MASTER_OFFSET)
+        )
     }
 }
